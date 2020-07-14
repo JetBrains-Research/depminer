7d5d54b941e532ba95ae7db7ce4f2bad38d4126a
Vladimir Kovalenko <vladimir.kovalenko@jetbrains.com>
2020-07-14 19:33:47 +0200

    introduce analysis scope

--- src/main/kotlin/org/jetbrains/research/depminer/actions/ProcessProject.kt
+++ src/main/kotlin/org/jetbrains/research/depminer/actions/ProcessProject.kt
@@ -1,7 +1,13 @@
 package org.jetbrains.research.depminer.actions
 
+import org.jetbrains.research.depminer.model.AnalysisScope
 import org.jetbrains.research.depminer.model.Dependency
+import org.jetbrains.research.depminer.model.ProjectScope
 
 fun getProjectDependencies(projectPath: String): Collection<Dependency> {
+    return getDependencies(ProjectScope(projectPath))
+}
+
+private fun getDependencies(scope: AnalysisScope): Collection<Dependency> {
     return emptyList()
 }
--- src/main/kotlin/org/jetbrains/research/depminer/model/Model.kt
+++ src/main/kotlin/org/jetbrains/research/depminer/model/Model.kt
@@ -15,8 +15,28 @@ data class Dependency(val type: ConnectionType, val from: CodeElement, val to: C
 
 data class FileLocation(val line: Int, val offset: Int)
 
-data class FileRange(val start: FileLocation, val end: FileLocation)
+data class FileRange(val start: FileLocation?, val end: FileLocation?)
 
-data class LocationInfo(val path: String, val range: FileRange)
+data class LocationInfo(val path: String, val range: FileRange): AnalysisScope {
+    override fun getLocations(): List<LocationInfo> {
+        return listOf(this)
+    }
+}
 
 data class CodeElement(val physicalLocation: LocationInfo)
+
+interface AnalysisScope {
+    fun getLocations(): List<LocationInfo>
+}
+
+class FileScope(val path: String): AnalysisScope {
+    override fun getLocations(): List<LocationInfo> {
+        return listOf(LocationInfo(path, FileRange(null, null)))
+    }
+}
+
+class ProjectScope(val path: String): AnalysisScope {
+    override fun getLocations(): List<LocationInfo> {
+        TODO("Not yet implemented")
+    }
+}
