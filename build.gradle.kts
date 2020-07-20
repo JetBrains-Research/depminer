035cd01c8646152ca770c43ac76fad646b12c7f3
Vladimir Kovalenko <vladimir.kovalenko@jetbrains.com>
2020-07-20 15:49:59 +0200

    change build.gradle to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("java")
+    id("idea")
+    id("org.jetbrains.intellij") version "0.4.21"
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("junit:junit:4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2020.1.2"
+    setPlugins("java", "Kotlin")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    runIde {
+        val dataset: String? by project
+        val output: String? by project
+        args = listOfNotNull("mine-dependencies", dataset, output)
+    }
+
+    register("extractDependencies") {
+        dependsOn(runIde)
+    }
+}
+
