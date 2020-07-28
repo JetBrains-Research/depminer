b3fcfc56d0920bd70f639662ee3f958ec4fc51b8
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-07-28 16:56:19 +0300

    Test case corrected

--- src/test/kotlin/BasicTest.kt
+++ src/test/kotlin/BasicTest.kt
@@ -17,12 +17,12 @@ class BasicTest {
     @Test
     fun `Project Scope is Derived From Project Path Correctly`() {
         val projectPath = System.getProperty("user.dir")
-        val projectScope = ProjectScope("$projectPath/testData/testProjects/kotlinIdea")
+        val projectScope = ProjectScope("$projectPath/testData/testProjects/kotlinTestProject")
         val desiredProjectScope = listOf(
-            LocationInfo("$projectPath/testData/testProjects/kotlinIdea/src/Main.kt", FileRange(null, null)),
-            LocationInfo("$projectPath/testData/testProjects/kotlinIdea/src/Util.kt", FileRange(null, null)))
+            LocationInfo("$projectPath/testData/testProjects/kotlinTestProject/src/Main.kt", FileRange(null, null)),
+            LocationInfo("$projectPath/testData/testProjects/kotlinTestProject/src/Utility.kt", FileRange(null, null)))
         Assert.assertTrue(
-            "Two kotlin files found: Main.kt and Util.kt",
+            "Two kotlin files found: Main.kt and Utility.kt",
             projectScope.getLocations() == desiredProjectScope
         )
     }
