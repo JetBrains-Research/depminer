7b5746fa686506db167d1afc1d2e14f30694f717
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-07-28 20:09:08 +0300

    Trying to make Kotlin work with project sdk setup - WIP

--- src/main/kotlin/org/jetbrains/research/depminer/runner/ProjectSetupUtil.kt
+++ src/main/kotlin/org/jetbrains/research/depminer/runner/ProjectSetupUtil.kt
@@ -1,19 +1,36 @@
 package org.jetbrains.research.depminer.runner
 
+import com.intellij.ide.impl.NewProjectUtil
 import com.intellij.ide.impl.ProjectUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.JavaSdk
+import com.intellij.openapi.projectRoots.ProjectJdkTable
+import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VfsUtilCore
 import testOutput
 import java.io.File
+import java.lang.System.*
 import kotlin.system.exitProcess
 
 fun projectSetup(inputDir: File, outputDir: File): Project {
+
+    val isKotlin = false //Work in progress
+
     val project = loadProject(inputDir.absolutePath, outputDir)
-
     println("Successfully opened project at inputDir: $project")
-    println("Project setup debug info:")
 
+    println("Project setup debug info:")
+    visitProjectFiles(project)
+
+    if (isKotlin) {
+        setupKotlinSDK(project)
+    }
+
+    return project
+}
+
+fun visitProjectFiles(project: Project) {
     ProjectRootManager.getInstance(project).contentRoots.forEach { root ->
         println("Project root: $root")
         VfsUtilCore.iterateChildrenRecursively(root, null) {
@@ -21,6 +38,13 @@ fun projectSetup(inputDir: File, outputDir: File): Project {
             true
         }
     }
+}
+
+fun setupKotlinSDK(project: Project): Project {
+    val sdk = JavaSdk.getInstance().createJdk("openjdk", getenv("JAVA_HOME")!!, true)
+    ProjectJdkTable.getInstance().addJdk(sdk)
+    ProjectRootManager.getInstance(project).projectSdk = sdk
+    NewProjectUtil.applyJdkToProject(project, sdk)
     return project
 }
 
