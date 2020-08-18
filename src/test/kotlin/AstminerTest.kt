b58319e46100dea889983521e8b280d5d16b3858
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-18 14:58:58 +0300

    AstminerTest.kt passes

--- src/test/kotlin/AstminerTest.kt
+++ src/test/kotlin/AstminerTest.kt
@@ -14,7 +14,7 @@ class AstminerTest {
     }
 
     @Test
-    fun `Astminer-master-dev Dependencies Mining Test`() {
+    fun `Astminer-master-dev Granularity_kt Dependencies Mining Test`() {
         val testInputPath = "testData/testProjects/astminer-master-dev/"
         val testSrcPath = "testData/testProjects/astminer-master-dev/src/"
 
@@ -27,10 +27,30 @@ class AstminerTest {
                 "JavaMethodSplitter.kt", "PythonMethodSplitter.kt", "FuzzyMethodSplitter.kt", "FuzzyNode.kt", "GumTreeJavaNode.kt", "GumTreeMethodSplitter.kt")
 
         val dependencies = readFromJsonString(readTestFile())
-        val dependenciesGranularityKt = dependencies.filter {it.from.location.isInFile(consideredFile) || it.to.location.isInFile(consideredFile)}
+        val dependenciesGranularityKt = dependencies.filter {it.from.location.isInFile(consideredFile)}
         println(dependenciesGranularityKt)
         for (dependency in dependenciesGranularityKt) {
-            assertTrue { dependency.to.location.path.substringAfterLast('/') in targetFiles || dependency.from.location.path.substringAfterLast('/') in targetFiles }
+            assertTrue {/* dependency.from.location.path.substringAfterLast('/') in targetFiles || */dependency.to.location.path.substringAfterLast('/') in targetFiles || dependency.to.location.path.substringAfterLast('/') == consideredFile }
+        }
+    }
+
+    @Test
+    fun `Astminer-master-dev ParsingModel_kt Dependencies Mining Test`() {
+        val testInputPath = "testData/testProjects/astminer-master-dev/"
+        val testSrcPath = "testData/testProjects/astminer-master-dev/src/"
+
+        println("Run IDE:")
+        val exitCode = runIde(testInputPath, testSrcPath, ".")
+        Assert.assertEquals("The IDE should finish terminate with code 0",0, exitCode)
+
+        val consideredFile = "ParsingModel.kt"
+        val targetFiles = listOf("kotlin.kotlin_builtins", "StandardKt.class", "CollectionsKt.class", "collections.kotlin_builtins", "ConsoleKt.class", "FilesKt.class", "SequencesKt.class", "StringsKt.class")
+
+        val dependencies = readFromJsonString(readTestFile())
+        val dependenciesGranularityKt = dependencies.filter {it.from.location.isInFile(consideredFile)}
+        println(dependenciesGranularityKt)
+        for (dependency in dependenciesGranularityKt) {
+            assertTrue {/* dependency.from.location.path.substringAfterLast('/') in targetFiles || */dependency.to.location.path.substringAfterLast('/') in targetFiles || dependency.to.location.path.substringAfterLast('/') == consideredFile }
         }
     }
 }
