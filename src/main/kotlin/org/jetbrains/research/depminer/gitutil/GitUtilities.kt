5f9a389a4029d3589d6dd3d28171030af5b524a6
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-09-02 20:31:02 +0300

    Add remote repository cloning

--- src/main/kotlin/org/jetbrains/research/depminer/gitutil/GitUtilities.kt
+++ src/main/kotlin/org/jetbrains/research/depminer/gitutil/GitUtilities.kt
@@ -1,17 +1,17 @@
 package org.jetbrains.research.depminer.gitutil
 
+import currentGitRepoBase
 import org.eclipse.jgit.api.Git
 import org.jetbrains.research.depminer.model.Review
 import java.io.File
 
-const val clonePath = "clonedRepos/"
 
-fun cloneRemoteRepository(newReview: Review): Git {
-    val remoteURI = newReview.commitInfo.project + ".git"
+fun cloneRemoteRepository(newReview: Review, dirPath: String): Git {
+    val remoteURI = currentGitRepoBase + newReview.commitInfo.project + ".git"
     println("Cloning repository from%${remoteURI}")
     val git = Git.cloneRepository()
         .setURI(remoteURI)
-        .setDirectory(File(System.getProperty("user.dir") + clonePath))
+        .setDirectory(File(dirPath))
         .call()
     println("Cloned repository into:${git.repository.directory.absolutePath}")
     return git
--- src/main/kotlin/org/jetbrains/research/depminer/runner/IdeRunner.kt
+++ src/main/kotlin/org/jetbrains/research/depminer/runner/IdeRunner.kt
@@ -1,6 +1,7 @@
 import com.intellij.openapi.application.ApplicationStarter
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
+import com.jetbrains.python.sdk.path
 import org.jetbrains.research.depminer.gitutil.cloneRemoteRepository
 import org.jetbrains.research.depminer.model.convertToJsonString
 import org.jetbrains.research.depminer.model.parseReviewHistory
@@ -14,6 +15,9 @@ import kotlin.system.exitProcess
  */
 const val testOutput = "test-output"
 
+const val currentGitRepoBase = "https://opendev.org/"
+const val clonePath = "/clonedRepo/"
+
 
 class IdeRunner : ApplicationStarter {
 
@@ -26,23 +30,24 @@ class IdeRunner : ApplicationStarter {
         var inputDir: File? = null
         var sourceRootDir: File? = null
         if (args[1].endsWith("review-mode")) {
+            val pathToRepository = args[1].substringBefore("review-mode")
             val reviewHistoryPath = args[2]
             println("Looking for review history file at:${File(projectPath).resolve(reviewHistoryPath).absolutePath}")
             val reviewHistory = parseReviewHistory(File(projectPath).resolve(reviewHistoryPath).absolutePath)
             reviewHistory.reverse()
             val newReview = reviewHistory[reviewHistory.lastIndex]
             println("Review: $newReview is chosen as the new review for analysis")
-            val git = cloneRemoteRepository(newReview)
+            val git = cloneRemoteRepository(newReview, pathToRepository + clonePath)
             inputDir = git.repository.directory
             sourceRootDir = git.repository.directory
         } else {
             inputDir = File(projectPath).resolve(args[1])
-            println(inputDir.absolutePath) //Debug
+            println(inputDir.absolutePath)
             sourceRootDir = File(projectPath).resolve(args[2])
             println(sourceRootDir.absolutePath)
         }
         val outputDir = File(projectPath).resolve(args[3])
-        println(outputDir.absolutePath) //Debug
+        println(outputDir.absolutePath)
 
         val project = projectSetup(inputDir!!, sourceRootDir!!, outputDir)
 
